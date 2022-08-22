24f36005007c453afd3d932deeb8e153f1dd9e6a
monikagorkhiya <monikagorkhiya@users.noreply.github.com>
2022-08-22 13:25:59 +0000

    deploy: 7a32a26ffcdfd4e07dd456c7a97cdba19e1ff445

--- _nuxt/static/1660127126/services/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660127126",layout:"theme",error:a,state:a,serverRendered:false,routePath:"\u002Fservices",config:{MODE:"staging",BASE_URL:b,DATA_URL:b,_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"https:\u002F\u002Fdata.improwised.com"));
