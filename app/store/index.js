4afff0213ec85c4cd2ad78167c77d9ca72ac48e7
Tapan Bavaliya <tapanbavaliya@users.noreply.github.com>
2022-09-30 13:31:27 +0530

    serve pdf statically (#132)

--- app/store/index.js
+++ app/store/index.js
@@ -2,11 +2,15 @@ export const strict = false;
 
 export const state = () => ({
   services: [],
+  generate: false,
 });
 
 export const mutations = {
   services(state, services) {
     state.services = services;
+  },
+  generate(state, generate) {
+    state.generate = generate;
   },
 };
 
@@ -14,6 +18,9 @@ export const actions = {
   async nuxtServerInit({ dispatch, commit }) {
     await dispatch("servicesListWithTitleOnly").then((services) => {
       commit("services", services.data);
+      if (process.argv[2] && process.argv[2] === "generate") {
+        commit("generate", true);
+      }
     });
   },
 
