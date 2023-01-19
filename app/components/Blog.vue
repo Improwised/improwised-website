21a5f9226eb04699adaaa8b8e581a933da14e0c9
Monika Gorkhiya <97655102+monikagorkhiya@users.noreply.github.com>
2023-01-19 17:21:34 +0530

    fix: add changes of blog (#153)

--- app/components/Blog.vue
+++ app/components/Blog.vue
@@ -8,7 +8,7 @@
       <article class="feature feature-3 blog-feature boxed boxed--border m-1">
         <div class="feature__body">
           <h4>{{ blogs.title }}</h4>
-          <p class="pb-5">{{ blogs.description }}</p>
+          <p class="pb-5" v-html="blogs.description"></p>
 
           <span class="font-14 blog_sub_details_left">{{
             blogs.date_created | formatDateTime
