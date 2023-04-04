97de8239d87823fdf113b8df1109c8e26ce665b8
tapanbavaliya <tbavaliya@gmail.com>
2023-04-04 21:14:23 +0530

    added google analytics

---
+++ app/components/GoogleAnalyticsV4.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-33PP6GNTHT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-33PP6GNTHT');
+</script>
+	</div>
+</template>
