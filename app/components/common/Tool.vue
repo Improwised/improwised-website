49ea7595bf2c6c468d9498133d04931d85c388cc
Payal Raviya <87559461+payalraviya@users.noreply.github.com>
2024-02-01 14:44:25 +0530

    fix: add tool component and used it in home page (#177)
    * fix: add tool component and used it in home page
    
    * fix: remove digital ocen class

---
+++ app/components/common/Tool.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="col-sm-3 col-md-2 col-xs-6 col-lg-2 col-6 tool">
+    <div class="boxed boxed--border text-center">
+      <img
+        :src="image"
+        :alt="name"
+        :title="name"
+        class="lazyload"
+        loading="lazy"
+      />
+      <h3 class="h6 remove-height">{{ name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      require: true,
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tool {
+  display: grid;
+}
+</style>
