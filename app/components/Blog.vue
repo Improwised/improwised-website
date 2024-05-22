c285f639907f255d7836ec63829b27ef0b1fd0d3
jinal-gajjar <jinal.gajjar@improwised.com>
2024-05-22 12:59:08 +0530

    fix: change redirection way

--- app/components/Blog.vue
+++ app/components/Blog.vue
@@ -5,11 +5,14 @@
       :key="index"
       class="col-lg-4 col-md-6 mb-1"
       style="cursor: pointer"
-      @click="navigateToBlog(blogs.slug, blogs.content)"
     >
       <article class="feature feature-3 blog-feature boxed boxed--border m-1">
         <div class="feature__body">
-          <h4>{{ blogs.title }}</h4>
+          <h4>
+            <a :href="`/blog/${blogs.slug}`" class="blog-title">{{
+              blogs.title
+            }}</a>
+          </h4>
           <p class="pb-5" v-html="blogs.description"></p>
 
           <span class="font-14 blog_sub_details_left">{{
@@ -29,12 +32,12 @@ export default {
       required: true,
     },
   },
-  methods: {
-    navigateToBlog(slug, content) {
-      if (content && content.length) {
-        window.location.href = `/blog/${slug}/`;
-      }
-    },
-  },
 };
 </script>
+
+<style scoped>
+.blog-title {
+  color: inherit;
+  font-weight: normal;
+}
+</style>
