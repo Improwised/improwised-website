2cb9af08ad33b085c980c8925986ee3b41e53590
jinal-gajjar <123358026+jinal-gajjar@users.noreply.github.com>
2024-05-23 18:46:29 +0530

    feat: added redirection at blog page. (#192)
    * feat: remove 'read more' from blog and added direct navigation for blog to blog detail page
    
    * fix: change redirection way
    
    * added bootstrap class for blog title

--- app/components/Blog.vue
+++ app/components/Blog.vue
@@ -4,22 +4,22 @@
       v-for="(blogs, index) in blogList"
       :key="index"
       class="col-lg-4 col-md-6 mb-1"
+      style="cursor: pointer"
     >
       <article class="feature feature-3 blog-feature boxed boxed--border m-1">
         <div class="feature__body">
-          <h4>{{ blogs.title }}</h4>
+          <h4>
+            <a
+              :href="`/blog/${blogs.slug}`"
+              class="blog-title text-dark font-weight-normal"
+              >{{ blogs.title }}</a
+            >
+          </h4>
           <p class="pb-5" v-html="blogs.description"></p>
 
           <span class="font-14 blog_sub_details_left">{{
             blogs.date_created | formatDateTime
           }}</span>
-          <a
-            v-if="blogs.content && blogs.content.length"
-            :href="`/blog/${blogs.slug}/`"
-            class="float-right blog_sub_details_right"
-          >
-            Read More
-          </a>
         </div>
       </article>
     </div>
