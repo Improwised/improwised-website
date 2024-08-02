e05b9053acf3c849356af8cc9221aca67b19a290
ankitjilka <ankitjilka10@gmail.com>
2024-08-02 12:01:09 +0530

    lhci and lighthouseci file added

---
+++ .lighthouserc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    ci: {
+        assert: {    
+            assertions: {
+                "offscreen-images": "off",  // audit will not check images which not visible to users.
+                "uses-webp-images": "off",  // audit will not check webp images
+                "first-contentful-paint": ["warn", {"maxNumericValue": 5000}],  // wait 5 seconds for render the first page
+                "interactive": ["error", {"maxNumericValue": 7000, "aggregationMethod": "optimistic"}],  // wait 7 seconds for fully interactive
+                "resource-summary:document:size": ["error", {"maxNumericValue": 14000}],  // if html doc size bigger than 14000 bytes give error
+                "categories:performance": ['warn', { minScore: 0.7 }],  // if overall performance score below 0.7 gives warning
+                "categories:accessibility": ['error', { minScore: 0.90 }],  // if accessibility is below 0.93 then gives error
+                "categories:best-practices": ['error', { minScore: 0.90 }],
+                "categories:seo": ["error", { "minScore": 0.90 }],
+                "categories:pwa": ["warn", { "minScore": 0.90 }]
+            },
+        },
+        collect: {
+            "numberOfRuns": 1,
+            "headful": false,
+            "chromePath": process.env.CHROME_PATH ,
+            "staticDistDir": './public',
+            "settings": {
+                "chromeFlags": "--no-sandbox --disable-dev-shm-usage"
+            }
+        },
+        upload: {
+            target: 'lhci',
+            serverBaseUrl: 'http://164.52.192.158:30011',
+            basicAuth: {
+                "username": "admin",
+                "password": "3mKBVGgxk5XtHvbZee90"
+            },
+            token: '6e9f0e54-ffe3-41c7-8f2e-9d67719afb93', // build-token which use for add data on server
+        },
+    },
+}
